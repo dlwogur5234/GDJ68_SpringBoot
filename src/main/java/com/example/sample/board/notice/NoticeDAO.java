fdc65987182e3efeda3c2af32fed213d30cc9a37
dlwogur5234 <dlwogur5234@naver.com>
2023-09-18 17:43:29 +0900

    230918-1

---
+++ src/main/java/com/example/sample/board/notice/NoticeDAO.java
@@ -0,0 +1,5 @@
+package com.example.sample.board.notice;
+
+public class NoticeDAO {
+
+}
---
+++ src/main/java/com/example/sample/board/notice/NoticeVO.java
@@ -0,0 +1,14 @@
+package com.example.sample.board.notice;
+
+import java.sql.Date;
+
+public class NoticeVO {
+
+	private Long boardNo;
+	private String boardTitle;
+	private String boardWriter;
+	private String boardContents;
+	private Date boardDate;
+	private int hit;
+	
+}
